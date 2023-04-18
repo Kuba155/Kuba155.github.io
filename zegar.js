a364646cdddb3e425d49cdc78c541b259f1ad65d
Kuba155 <kubawalasik21@gmail.com>
2023-04-18 21:04:24 +0200

    update 0.2.4

---
+++ zegar.js
@@ -0,0 +1,43 @@
+
+
+
+
+function czas2() {
+
+
+var dzis = new Date();
+
+var miesoc = dzis.getMonth()+1;
+if (miesoc<10) miesoc = "0"+miesoc;
+var dzien = dzis.getDay();
+if (dzien<10) dzien = "0"+dzien;
+var rok = dzis.getFullYear();
+
+
+document.getElementById("czas").innerHTML = miesoc+"/"+dzien+"/"+rok;
+
+
+setTimeout("czas2()",1000);
+
+
+
+}
+
+function stoper() {
+
+var czas = new Date();
+
+var godzina = czas.getHours();
+if (godzina<10) godzina = "0"+godzina;
+var minuta = czas.getMinutes();
+if (minuta<10) minuta = "0"+minuta;
+var sekunda = czas.getSeconds();
+if (sekunda<10) sekunda = "0"+sekunda;
+
+document.getElementById("12").innerHTML = godzina+":"+minuta+":"+sekunda;
+
+setTimeout("stoper()",1000)
+
+
+}
+
